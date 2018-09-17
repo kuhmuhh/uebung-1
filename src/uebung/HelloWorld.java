2d94b9a8cacfb4e1664e82541aef609080a9dfa6
Hetzel.Andreas <Hetzel.Andreas@SK-448A5BCB8A90.2631.skool.net>
2018-09-17 15:33:31 +0200

    karlnice

--- src/uebung/HelloWorld.java
+++ src/uebung/HelloWorld.java
@@ -4,7 +4,22 @@ public class HelloWorld {
 
 	
 	public static void main(String[] args) {
-		System.out.println("Hello World");
+		
+		int zahl = 7;
+		
+		while(zahl>=1)
+		
+		
+		if(zahl%2 !=0){
+			zahl = zahl*3+1;
+	
+		}
+		else{zahl = zahl/2;}
+		
+	System.out.print(zahl+", ");
+	
 	}
+	
+	
 
-}
+	}
